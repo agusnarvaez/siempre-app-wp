41652007ad63154df90d41f7a66ea76161f34b48
agusnarvaez <agus.narvaez@outlook.com>
2025-11-04 14:20:23 -0300

    Agrego redirección a index desde el header

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,9 +1,16 @@
-import { Outlet } from "react-router-dom"
+import { Outlet,useNavigate } from "react-router-dom"
 import siempreLogo from '../../assets/logos/logo-siempre.png'
 
 export default function Main(){
+    /* Hacer función ir a home con useNavigate */
+    const navigate = useNavigate()
+
+    const goHome = () => {
+        navigate('/') // redirige al PackagesForm
+    }
+
     return <main className="page-content">
-        <header className='main-header'><img src={siempreLogo} alt="Logo Siempre" /></header>
+        <header className='main-header'><img onClick={goHome} src={siempreLogo} alt="Logo Siempre" /></header>
         <main className="main-content">
             <Outlet />
         </main>
